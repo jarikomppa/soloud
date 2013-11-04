b926708ac718a8ec0ef1f7addf6dfc3c58cf9205
jarikomppa <jari.komppa@gmail.com>
2013-11-04 21:39:33 +0200

    doc toolchain now does website and docs in html, pdf and epub formats.

--- doc/makedoc.bat
+++ doc/makedoc.bat
@@ -1,3 +1,29 @@
+@echo off
+call pandoc -s --toc --default-image-extension=png -o soloud.html intro.mmd legal.mmd quickstart.mmd concepts.mmd faq.mmd basics.mmd attributes.mmd faders.mmd coremisc.mmd audiosource.mmd wav.mmd wavstream.mmd speech.mmd newsoundsources.mmd mixbus.mmd filters.mmd biquadfilter.mmd echofilter.mmd fftfilter.mmd backends.mmd
+
+call pandoc -B htmlpre.txt -A htmlpost.txt --default-image-extension=png intro.mmd -o index.html
+call pandoc -B htmlpre.txt -A htmlpost.txt --default-image-extension=png legal.mmd -o legal.html
+call pandoc -B htmlpre.txt -A htmlpost.txt --default-image-extension=png downloads.mmd -o downloads.html
+call pandoc -B htmlpre.txt -A htmlpost.txt --default-image-extension=png quickstart.mmd -o quickstart.html
+call pandoc -B htmlpre.txt -A htmlpost.txt --default-image-extension=png concepts.mmd -o concepts.html
+call pandoc -B htmlpre.txt -A htmlpost.txt --default-image-extension=png faq.mmd -o faq.html
+call pandoc -B htmlpre.txt -A htmlpost.txt --default-image-extension=png basics.mmd -o basics.html
+call pandoc -B htmlpre.txt -A htmlpost.txt --default-image-extension=png attributes.mmd -o attributes.html
+call pandoc -B htmlpre.txt -A htmlpost.txt --default-image-extension=png faders.mmd -o faders.html
+call pandoc -B htmlpre.txt -A htmlpost.txt --default-image-extension=png coremisc.mmd -o coremisc.html
+call pandoc -B htmlpre.txt -A htmlpost.txt --default-image-extension=png audiosource.mmd -o audiosource.html
+call pandoc -B htmlpre.txt -A htmlpost.txt --default-image-extension=png wav.mmd -o wav.html
+call pandoc -B htmlpre.txt -A htmlpost.txt --default-image-extension=png wavstream.mmd -o wavstream.html
+call pandoc -B htmlpre.txt -A htmlpost.txt --default-image-extension=png speech.mmd -o speech.html
+call pandoc -B htmlpre.txt -A htmlpost.txt --default-image-extension=png newsoundsources.mmd -o newsoundsources.html
+call pandoc -B htmlpre.txt -A htmlpost.txt --default-image-extension=png mixbus.mmd -o mixbus.html 
+call pandoc -B htmlpre.txt -A htmlpost.txt --default-image-extension=png filters.mmd -o filters.html
+call pandoc -B htmlpre.txt -A htmlpost.txt --default-image-extension=png biquadfilter.mmd -o biquadfilter.html
+call pandoc -B htmlpre.txt -A htmlpost.txt --default-image-extension=png echofilter.mmd -o echofilter.html
+call pandoc -B htmlpre.txt -A htmlpost.txt --default-image-extension=png fftfilter.mmd -o fftfilter.html
+call pandoc -B htmlpre.txt -A htmlpost.txt --default-image-extension=png backends.mmd -o backends.html
+
+call pandoc -N --toc  --epub-cover-image=images/cover.png -t epub3 --default-image-extension=png -S --epub-stylesheet=epub.css --epub-metadata=metadata.xml -o SoLoud.epub title.txt intro.mmd legal.mmd quickstart.mmd concepts.mmd faq.mmd basics.mmd attributes.mmd faders.mmd coremisc.mmd audiosource.mmd wav.mmd wavstream.mmd speech.mmd newsoundsources.mmd mixbus.mmd filters.mmd biquadfilter.mmd echofilter.mmd fftfilter.mmd backends.mmd
 call pandoc --listings --default-image-extension=pdf --chapters attributes.mmd -o attributes.tex 
 call pandoc --listings --default-image-extension=pdf --chapters audiosource.mmd -o audiosource.tex
 call pandoc --listings --default-image-extension=pdf --chapters backends.mmd -o backends.tex
@@ -18,7 +44,8 @@ call pandoc --listings --default-image-extension=pdf --chapters quickstart.mmd -
 call pandoc --listings --default-image-extension=pdf --chapters speech.mmd -o speech.tex
 call pandoc --listings --default-image-extension=pdf --chapters wav.mmd -o wav.tex
 call pandoc --listings --default-image-extension=pdf --chapters wavstream.mmd -o wavstream.tex
-del *.aux *.toc *.out *.log
+del *.aux *.toc *.out *.log *.lg *.4ct *.4tc *.idv *.tmp *.xdv *.xref
 xelatex SoLoud.tex
 xelatex SoLoud.tex
-start soloud.pdf
+echo ---------------
+echo soloud.pdf soloud.epub soloud.html + bunch of .html files

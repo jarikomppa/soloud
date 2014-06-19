7c2e1388a6b9e0347f1d73b27f8d5dd4304ca02e
jarikomppa <jari.komppa@gmail.com>
2014-06-19 20:33:05 +0300

    Helper script that finds undocumented API functions

---
+++ glue/checkapidoc.py
@@ -0,0 +1,45 @@
+""" Complains if some API call is not documented """
+
+import soloud_codegen
+import glob
+import os
+
+def checkfile(apifunc, fname):
+    """ Checks whether the string can be found in a file """
+    if apifunc in open(fname).read():
+        #print apifunc + " found in " + fname
+        return True
+    return False
+    
+def checkfiles(apifunc):
+    """ Goes through all multimarkdown files in doc dir and passes
+        them to checkfile. If found, early out.
+    """
+    for file in glob.glob("../doc/*.mmd"):
+        if checkfile(apifunc, file):
+            return True
+    return False
+
+print "Checking for undocumented APIs.."
+print
+
+found = 0
+
+for func in soloud_codegen.soloud_func:
+    apifunc = func[1].replace("_",".")
+    if ((apifunc[-2::] != "Ex") and 
+    	  (apifunc[-7::] != "destroy") and 
+    	  (apifunc[-6::] != "create")):
+        if not checkfiles(apifunc):
+            print apifunc
+            found += 1
+
+if found == 0:
+    print "All good!"
+else:
+    print
+    print str(found) + " APIs undocumented. Get to work!"
+
+  
+
+        

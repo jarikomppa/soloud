3d7948a8c7e526a41ea6a0bc4e4dec4fa7c711a9
jarikomppa <jari.komppa@gmail.com>
2014-06-21 21:30:04 +0300

    Simple python test

---
+++ glue/test_python.py
@@ -0,0 +1,27 @@
+from soloud import *
+
+with Soloud() as audiolib:
+	audiolib.init()
+	audiolib.set_global_volume(10)
+	
+	speech = Speech()
+	
+	flanger = FlangerFilter()
+	speech.set_filter(0, flanger)		
+	
+	t = "Hello Python (OOP) World!"
+	speech.set_text(t)
+	
+	print(t)
+	audiolib.play(speech)
+
+	print "Enter text to speak (empty string quits)"
+	while t != "":
+		t = raw_input(": ")
+
+		speech.set_text(t);
+		audiolib.play(speech);
+
+	speech.close()
+
+print "Bye"

683da4092d2d867f8ba5b110a0674b66864a1fa3
jarikomppa <jari.komppa@gmail.com>
2015-05-11 20:29:12 +0300

    Web page changed to same kind of layout as the pdf

--- doc/makedoc.py
+++ doc/makedoc.py
@@ -81,7 +81,7 @@ if not os.path.exists("temp/"):
     
 print "- -- --- -- - Generating single-file HTML docs"
 
-callp = ["pandoc", "-s", "--toc", "--default-image-extension=png", "-o", datestring + "/soloud_" + datestring + ".html"]
+callp = ["pandoc", "-s", "-t", "html5", "-H", "singlehtml_head.txt", "-B", "singlehtml_body.txt", "--toc", "--self-contained", "--default-image-extension=png", "-o", datestring + "/soloud_" + datestring + ".html"]
 for x in src:
     if x not in website_only:
         callp.append(x)
@@ -93,7 +93,7 @@ for x in src:
     with open(datestring + "/web/" + x[:len(x)-3]+"html", "w") as file_out:
         with open(datestring + "/web/" + x[:len(x)-3]+"html.bak", "r") as file_in:
             for line in file_in:
-                file_out.write(line.replace('code>', 'code>\n'))
+                file_out.write(line.replace('code>', 'code>\n').replace('::','::<wbr>'))
     if x == "intro.mmd":
         if os.path.isfile(datestring + "/web/index.html"):
             os.remove(datestring + "/web/index.html")

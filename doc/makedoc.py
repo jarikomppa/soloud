87134b73fdc87686e646c8e790d3fde5ca82718b
jarikomppa <jari.komppa@gmail.com>
2015-05-12 22:36:31 +0300

    Updated downloads page. Also, pandoc apparently spews random characters as nonbreaking space in web pages. Fun.

--- doc/makedoc.py
+++ doc/makedoc.py
@@ -95,7 +95,7 @@ for x in src:
     with open(datestring + "/web/" + x[:len(x)-3]+"html", "w") as file_out:
         with open(datestring + "/web/" + x[:len(x)-3]+"html.bak", "r") as file_in:
             for line in file_in:
-                file_out.write(line.replace('code>', 'code>\n').replace('::','::<wbr>').replace('\xc2',''))
+                file_out.write(line.replace('code>', 'code>\n').replace('::','::<wbr>').replace('\xc2','').replace('\xa0',''))
     if x == "intro.mmd":
         if os.path.isfile(datestring + "/web/index.html"):
             os.remove(datestring + "/web/index.html")

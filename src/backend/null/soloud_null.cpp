f7fda62cb47a102c5773d244e7fed56d8df7fe1c
jarikomppa <jari.komppa@gmail.com>
2015-05-20 21:19:26 +0300

    Don't allow unsupported channel settings (like 3, 5 or 27 channels)

--- src/backend/null/soloud_null.cpp
+++ src/backend/null/soloud_null.cpp
@@ -44,12 +44,14 @@ namespace SoLoud
 
     result null_init(Soloud *aSoloud, unsigned int aFlags, unsigned int aSamplerate, unsigned int aBuffer, unsigned int aChannels)
     {
+		if (aChannels == 0 || aChannels == 3 || aChannels == 5 || aChannels > 6 || aBuffer < SAMPLE_GRANULARITY)
+			return INVALID_PARAMETER;
         aSoloud->mBackendData = 0;
         aSoloud->mBackendCleanupFunc = nullCleanup;
 
         aSoloud->postinit(aSamplerate, aBuffer, aFlags, aChannels);
         aSoloud->mBackendString = "null driver";
-        return 0;
+        return SO_NO_ERROR;
     }
 };
 #endif

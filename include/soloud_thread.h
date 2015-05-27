5f731a585aef55c175f05edad4c5c66481da9d47
jarikomppa <jari.komppa@gmail.com>
2015-05-27 18:48:12 +0300

    added implementation of a thread pool

--- include/soloud_thread.h
+++ include/soloud_thread.h
@@ -46,6 +46,37 @@ namespace SoLoud
 		void sleep(int aMSec);
         void wait(ThreadHandle aThreadHandle);
         void release(ThreadHandle aThreadHandle);
+
+#define MAX_THREADPOOL_TASKS 1024
+
+		class PoolTask
+		{
+		public:
+			virtual void work() = 0;
+		};
+
+		class Pool
+		{
+		public:
+			volatile int mRunning; // running flag, used to flag threads to stop
+			int mThreadCount; // number of threads
+			ThreadHandle *mThread; // array of thread handles
+			void *mWorkMutex; // mutex to protect task array/maxtask
+			PoolTask *mTaskArray[MAX_THREADPOOL_TASKS]; // pointers to tasks
+			int mMaxTask; // how many tasks are pending
+			int mRobin; // cyclic counter, used to pick jobs for threads
+
+			// Initialize and run thread pool. For thread count 0, work is done at addWork call.
+			void init(int aThreadCount);
+			// Ctor, sets known state
+			Pool();
+			// Dtor. Waits for the threads to finish. Work may be unfinished.
+			~Pool();
+			// Add work to work list. Object is not automatically deleted when work is done.
+			void addWork(PoolTask *aTask);
+			// Called from worker thread to get a new task. Returns null if no work available.
+			PoolTask *getWork();
+		};
 	}
 }
 

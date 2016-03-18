e502935cfa193e568db06f0d35edfba8b9e7b138
Amir Ramezani <amir.ramezani1370@gmail.com>
2016-03-18 12:54:33 +0330

    added SoLoud::PhysfsFile to work with PhysicsFS filesystem library

---
+++ include/soloud_physfs.h
@@ -0,0 +1,56 @@
+/*
+SoLoud audio engine
+Copyright (c) 2013-2015 Jari Komppa
+
+file class for PhysicsFS filesystem
+Copyright (se) 2016 amir ramezani
+
+This software is provided 'as-is', without any express or implied
+warranty. In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+   1. The origin of this software must not be misrepresented; you must not
+   claim that you wrote the original software. If you use this software
+   in a product, an acknowledgment in the product documentation would be
+   appreciated but is not required.
+
+   2. Altered source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+   3. This notice may not be removed or altered from any source
+   distribution.
+*/
+
+ #ifndef SOLOUD_PHYSFS_H
+#define SOLOUD_PHYSFS_H
+
+#include <physfs.h>
+#include "soloud_file.h"
+
+namespace SoLoud
+{
+
+//this class is used for reading from and writing into physics FS files
+class PhysfsFile: public File
+{
+public:
+PHYSFS_File *mFileHandle;
+
+virtual int eof();
+virtual unsigned int read(unsigned char *aDst, unsigned int aBytes);
+virtual unsigned int length();
+virtual void seek(int aOffset);
+virtual unsigned int pos();
+virtual ~PhysfsFile();
+PhysfsFile();
+PhysfsFile(PHYSFS_File *fp);
+result open(const char *aFilename);
+virtual PHYSFS_File *getPhysfsFilePtr();
+};
+};
+
+#endif
---
+++ src/core/soloud_physfs.cpp
@@ -0,0 +1,94 @@
+/*
+SoLoud audio engine
+Copyright (c) 2013-2015 Jari Komppa
+
+file class for physicsFS filesystem
+Copyright (se) 2016 amir ramezani
+
+This software is provided 'as-is', without any express or implied
+warranty. In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+   1. The origin of this software must not be misrepresented; you must not
+   claim that you wrote the original software. If you use this software
+   in a product, an acknowledgment in the product documentation would be
+   appreciated but is not required.
+
+   2. Altered source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+   3. This notice may not be removed or altered from any source
+   distribution.
+*/
+
+#include "soloud_error.h"
+#include "soloud_physfs.h"
+
+namespace SoLoud
+{
+
+PhysfsFile::PhysfsFile()
+{
+PHYSFS_close(mFileHandle);
+}
+
+PhysfsFile::PhysfsFile(PHYSFS_File *fp):
+mFileHandle(fp)
+{
+
+}
+
+int PhysfsFile::eof()
+{
+return PHYSFS_eof(mFileHandle);
+}
+
+unsigned int PhysfsFile::read(unsigned char *aDst, unsigned int aBytes)
+{
+return PHYSFS_read(mFileHandle, aDst, aBytes, 1);
+}
+
+unsigned int PhysfsFile::length()
+{
+return PHYSFS_fileLength(mFileHandle);
+}
+
+void PhysfsFile::seek(int aOffset)
+{
+PHYSFS_seek(mFileHandle, aOffset);
+}
+
+unsigned int PhysfsFile::pos()
+{
+return PHYSFS_tell(mFileHandle);
+}
+
+PhysfsFile::~PhysfsFile()
+{
+PHYSFS_close(mFileHandle);
+}
+
+result PhysfsFile::open(const char *aFilename)
+{
+if(!PHYSFS_exists(aFilename))
+{
+return FILE_NOT_FOUND;
+}
+mFileHandle=PHYSFS_openRead(aFilename);
+if(!mFileHandle)
+{
+return FILE_LOAD_FAILED;
+}
+return SO_NO_ERROR;
+}
+
+PHYSFS_File *PhysfsFile::getPhysfsFilePtr()
+{
+return mFileHandle;
+}
+
+}

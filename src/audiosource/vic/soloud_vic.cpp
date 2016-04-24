de8388ba75761dff7611932c7576b0e853391ef6
jarikomppa <jari.komppa@gmail.com>
2016-04-24 14:10:02 +0300

    Fix warning from msvc (negating unsigned value)

--- src/audiosource/vic/soloud_vic.cpp
+++ src/audiosource/vic/soloud_vic.cpp
@@ -112,7 +112,7 @@ namespace SoLoud
 		{
 		    unsigned lsb = lfsr & 1;
 		    lfsr >>= 1;
-		    lfsr ^= (-lsb) & 0xB400u;
+		    lfsr ^= (unsigned)(-(signed)lsb) & 0xB400u;
 		    m_noise[i] = (lfsr & 0xff) ^ (lfsr >> 8);
 		}
 	}

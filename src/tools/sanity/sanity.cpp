781fe73c616da9012ccb97f341c1a80a581e84c2
jarikomppa <jari.komppa@gmail.com>
2018-02-18 11:22:48 +0200

    filter tests

--- src/tools/sanity/sanity.cpp
+++ src/tools/sanity/sanity.cpp
@@ -417,6 +417,7 @@ void testPlay()
 	soloud.deinit();
 }
 
+
 // Test various 3d functions
 // 
 // Soloud.play3d
@@ -460,7 +461,99 @@ void test3d()
 // DCRemovalFilter.setParams
 void testFilters()
 {
-	// TODO (basically check if filter does anything)
+	float scratch[2048];
+	float ref[2048];
+	float ref2[2048];
+	SoLoud::result res;
+	SoLoud::Soloud soloud;
+	SoLoud::Wav wav;
+	testWave(wav);
+	SoLoud::BiquadResonantFilter biquad;
+	SoLoud::LofiFilter lofi;
+	SoLoud::EchoFilter echo;
+	SoLoud::BassboostFilter bass;
+	SoLoud::FlangerFilter flang;
+	SoLoud::DCRemovalFilter dc;
+	res = soloud.init(SoLoud::Soloud::CLIP_ROUNDOFF, SoLoud::Soloud::NULLDRIVER);
+	CHECK_RES(res);
+
+	soloud.play(wav);
+	soloud.mix(ref, 1000);
+	soloud.stopAll();
+
+	biquad.setParams(SoLoud::BiquadResonantFilter::LOWPASS, 8000, 2000, 5);
+	wav.setFilter(0, &biquad);
+	soloud.play(wav);
+	soloud.mix(ref2, 1000);
+	CHECK_BUF_DIFF(ref, ref2, 2000);
+	soloud.stopAll();
+	biquad.setParams(SoLoud::BiquadResonantFilter::HIGHPASS, 8000, 1000, 5);
+	soloud.play(wav);
+	soloud.mix(scratch, 1000);
+	CHECK_BUF_DIFF(ref2, scratch, 2000);
+	soloud.stopAll();
+
+	lofi.setParams(4000, 5);
+	wav.setFilter(0, &lofi);
+	soloud.play(wav);
+	soloud.mix(ref2, 1000);
+	CHECK_BUF_DIFF(ref, ref2, 2000);
+	soloud.stopAll();
+	lofi.setParams(2000, 3);
+	soloud.play(wav);
+	soloud.mix(scratch, 1000);
+	CHECK_BUF_DIFF(ref2, scratch, 2000);
+	soloud.stopAll();
+
+	echo.setParams(0.05f, 0.5f, 0.5f);
+	wav.setFilter(0, &echo);
+	soloud.play(wav);
+	soloud.mix(ref2, 1000);
+	CHECK_BUF_DIFF(ref, ref2, 2000);
+	soloud.stopAll();
+	echo.setParams(0.01f, 0.75f, 0.1f);
+	soloud.play(wav);
+	soloud.mix(scratch, 1000);
+	CHECK_BUF_DIFF(ref2, scratch, 2000);
+	soloud.stopAll();
+
+	bass.setParams(2);
+	wav.setFilter(0, &bass);
+	soloud.play(wav);
+	soloud.mix(ref2, 1000);
+	CHECK_BUF_DIFF(ref, ref2, 2000);
+	soloud.stopAll();
+	bass.setParams(10);
+	soloud.play(wav);
+	soloud.mix(scratch, 1000);
+	CHECK_BUF_DIFF(ref2, scratch, 2000);
+	soloud.stopAll();
+
+	flang.setParams(0.05f, 10);
+	wav.setFilter(0, &flang);
+	soloud.play(wav);
+	soloud.mix(ref2, 1000);
+	CHECK_BUF_DIFF(ref, ref2, 2000);
+	soloud.stopAll();
+	flang.setParams(0.005f, 5);
+	soloud.play(wav);
+	soloud.mix(scratch, 1000);
+	CHECK_BUF_DIFF(ref2, scratch, 2000);
+	soloud.stopAll();
+
+	dc.setParams(0.1f);
+	wav.setFilter(0, &dc);
+	soloud.play(wav);
+	soloud.mix(ref2, 1000);
+	CHECK_BUF_DIFF(ref, ref2, 2000);
+	soloud.stopAll();
+	dc.setParams(0.5f);
+	soloud.play(wav);
+	soloud.mix(scratch, 1000);
+	CHECK_BUF_DIFF(ref2, scratch, 2000);
+	soloud.stopAll();
+
+	soloud.deinit();
 }
 
 int main(int parc, char ** pars)
@@ -517,12 +610,10 @@ Soloud.isVoiceGroup
 Soloud.isVoiceGroupEmpty
 Soloud.update3dAudio
 Bus.setChannels
-Bus.setVolume
 Bus.setLooping
 Bus.stop
 Speech.setText
 Speech.setParams
-Speech.setVolume
 Speech.setLooping
 Speech.stop
 Wav.load
@@ -540,7 +631,6 @@ WavStream.loadFile
 WavStream.loadFileToMem
 WavStream.getLength
 WavStream.setLoopRange
-WavStream.setVolume
 WavStream.setLooping
 WavStream.stop
 Sfxr.resetParams
@@ -548,20 +638,17 @@ Sfxr.loadParams
 Sfxr.loadParamsMem
 Sfxr.loadParamsFile
 Sfxr.loadPreset
-Sfxr.setVolume
 Sfxr.setLooping
 Sfxr.stop
 Openmpt.load
 Openmpt.loadMem
 Openmpt.loadFile
-Openmpt.setVolume
 Openmpt.setLooping
 Openmpt.stop
 Monotone.setParams
 Monotone.load
 Monotone.loadMem
 Monotone.loadFile
-Monotone.setVolume
 Monotone.setLooping
 Monotone.stop
 TedSid.load
@@ -569,7 +656,6 @@ TedSid.loadToMem
 TedSid.loadMem
 TedSid.loadFileToMem
 TedSid.loadFile
-TedSid.setVolume
 TedSid.setLooping
 TedSid.stop
 
@@ -577,6 +663,13 @@ Not tested - abstract class
 AudioAttenuator.attenuate
 
 Not tested - the functionality is the same for all audio sources, tested for Wav:
+TedSid.setVolume
+Monotone.setVolume
+Openmpt.setVolume
+Sfxr.setVolume
+WavStream.setVolume
+Speech.setVolume
+Bus.setVolume
 Bus.setFilter
 Bus.set3dMinMaxDistance
 Bus.set3dAttenuation

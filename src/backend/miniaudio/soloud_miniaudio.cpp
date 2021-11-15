301655746666ce3e2ef13ed598f6715ada4e29f4
Jari Komppa <jari.komppa@gmail.com>
2021-11-15 20:37:01 +0200

    Using aBuffer on miniaudio lead to crashes; let's let miniaudio figure out its buffer size.

--- src/backend/miniaudio/soloud_miniaudio.cpp
+++ src/backend/miniaudio/soloud_miniaudio.cpp
@@ -64,7 +64,7 @@ namespace SoLoud
     result miniaudio_init(SoLoud::Soloud *aSoloud, unsigned int aFlags, unsigned int aSamplerate, unsigned int aBuffer, unsigned int aChannels)
     {
         ma_device_config config = ma_device_config_init(ma_device_type_playback);
-        config.periodSizeInFrames = aBuffer;
+        //config.periodSizeInFrames = aBuffer; // setting to aBuffer (like 2048) causes miniaudio to crash; let's just use the default.
         config.playback.format    = ma_format_f32;
         config.playback.channels  = aChannels;
         config.sampleRate         = aSamplerate;
